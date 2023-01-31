db727eea4d96407a96c380c35a0b7ca304d9f6b7
amanda <amandareisdw@gmail.com>
2023-01-31 18:17:06 -0300

    [SETUP][#11] - Changed test description

--- src/test/ci.test.ts
+++ src/test/ci.test.ts
@@ -2,7 +2,7 @@ import { sum } from '@main/sum/foo'
 import { SIX } from '@test/main/constant'
 
 describe('Testing Jest with CI', () => {
-  it('Should teste the imports', () => {
+  it('Should test the imports', () => {
     const sut = sum(3, 3)
     expect(sut).toBe(SIX)
   })
