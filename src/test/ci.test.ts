aaaf0d5cc5ec70655c2f993ed4dd68aaf41f16b8
amanda <amandareisdw@gmail.com>
2023-02-01 19:21:54 -0300

    [TEST][#15] - Created tests for environment and config alias

--- src/test/ci.test.ts
+++ src/test/ci.test.ts
@@ -1,9 +1,17 @@
-import { sum } from '@main/sum/foo'
+import { sum, TEN } from '@main/sum/foo'
 import { SIX } from '@test/main/constant'
+import { environment } from '@config/environment'
 
 describe('Testing Jest with CI', () => {
   it('Should test the imports', () => {
     const sut = sum(3, 3)
     expect(sut).toBe(SIX)
+  })
+  it('Sould test the environment', () => {
+    expect(TEN).toBe(10)
+  })
+  it('Should test directly the @config import', () => {
+    const ALWAYS_TRUE = environment.boolean
+    expect(ALWAYS_TRUE).toBe(true)
   })
 })
