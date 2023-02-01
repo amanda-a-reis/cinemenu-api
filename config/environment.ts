a26138e259b8dbe7668cdc7f50326f80b68ab8ba
amanda <amandareisdw@gmail.com>
2023-02-01 19:44:13 -0300

    [BUGFIX][#15] - Changed the env name that was incorrect

--- config/environment.ts
+++ config/environment.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config({ path: './.env' })
 
 const environment = {
-  number: Number(process.env.ENV_TEN),
+  number: Number(process.env.ENV_NUMBER),
   boolean: Boolean(process.env.ENV_BOOL)
 }
 
